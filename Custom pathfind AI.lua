fa9e9708d5ce589e08902c048192503d2ce9c8e1
Rapha <israphaelgo@gmail.com>
2025-11-03 19:48:22 -0400

    Should be good now

--- Custom pathfind AI.lua
+++ Custom pathfind AI.lua
@@ -1,4 +1,5 @@
 --!nocheck
+--services
 local PathFindingService = game:GetService("PathfindingService")
 local OOP = require(script.OOP)
 local WaypointClearConfig = require(workspace.Actor.WayPointClear.Config)
@@ -7,92 +8,104 @@ local WaypointClearConfig = require(workspace.Actor.WayPointClear.Config)
 local VisualRayEnabled = false
 local VisualLPSEnabled = true
 
+--registers the NPC
 local NPC = OOP.New(script.Parent, VisualRayEnabled, VisualLPSEnabled)
 
+--NPC body parts
 local char = NPC.Character
 local head = NPC.Head
 local Torso = NPC.Torso
 local Hum = NPC.Humanoid
 
+--raycast options
 local FovDistance = OOP.FovDistance
 local NumberOfRays = 1
 --debug options
 local CheckForLastLPSPosition = true --turn on for less lag
-
 local MAX_TRIES = 25
+
+--other variables
 local CatchPlayerTries = 0
 
 --pathfind
 
 task.spawn(function()
 	while task.wait(0.05) do
-		local TargetCharacter = char:GetAttribute("AggroPlayer") and workspace:FindFirstChild(char:GetAttribute("AggroPlayer"))
-		local LastPositionPlayerWasSeen = NPC.LastPositionPlayerWasSeen
-		local waypos = TargetCharacter and TargetCharacter.Torso.Position or LastPositionPlayerWasSeen
+		local TargetCharacter = char:GetAttribute("AggroPlayer") and workspace:FindFirstChild(char:GetAttribute("AggroPlayer")) --checks for aggro player
+		local LastPositionPlayerWasSeen = NPC.LastPositionPlayerWasSeen --last known pos of player
+		local waypos = TargetCharacter and TargetCharacter.Torso.Position or LastPositionPlayerWasSeen -- where to head if player was last seen
+		
+		--path creation
 		local path = PathFindingService:CreatePath({
 			WaypointSpacing = 8, --8 --15
 			AgentRadius = 1.5,
 			AgentHeight = 5.8,
 			AgentCanJump = true
 		})
-
+		--skip cycle if NPC is dead
 		if Hum.Health <= 0 then
 			continue
 		end
-
+		--clear last pos seen if reached and hasn't found player
 		if LastPositionPlayerWasSeen and (LastPositionPlayerWasSeen - Torso.Position).Magnitude <= 1.5 then
 			print("Player wasn't found in his last seen position")
 			NPC.LastPositionPlayerWasSeen = nil
 			LastPositionPlayerWasSeen = nil
 			char:SetAttribute("AggroPlayer", nil)
+			--clear variables
 		end
-
+		-- pathfind towards target
 		if char:GetAttribute("AggroPlayer") then
+			--roam variable, if it isn't a player it will be Roam
 			if char:GetAttribute("AggroPlayer") == "Roam" then
-				--ROAM THROUGH MAP
+				--ROAM THROUGH MAP WAYPOINTS
 				for i,v: Part in ipairs(OOP.OrderTable(OOP.RoamWayPoints:GetChildren())) do
 					if char:GetAttribute("AggroPlayer") ~= "Roam" then
 						Hum:MoveTo(Torso.Position) -- halt movement
 						path = nil
 						break
 					end
+					--make path towards waypoint
 					path:ComputeAsync(Torso.Position, v.Position)
 					local Waypoints = path:GetWaypoints()
 
-
+					--if the path is succesful, go through each waypoint
 					if path.Status == Enum.PathStatus.Success then
-						for n,y:PathWaypoint in Waypoints do
+						for n,y:PathWaypoint in Waypoints do --nesting, variable defining
 							if not char:GetAttribute("AggroPlayer") then
+								--if the aggro player attribute is cleared, stop pathfinding
 								print("give up gng :sob::finger_cross:")
 								Hum:MoveTo(Torso.Position)
 								break
 							end
 
 							if char:GetAttribute("AggroPlayer") ~= "Roam" then
+								-- if player has been found while roaming, stop pathfinding
 								Hum:MoveTo(Torso.Position) -- halt movement
 								path = nil
 								print("Found Player")
 								break
 							end
-
+							--if the magnitude to the waypoint from the torso is less or 4, break
 							if (Torso.Position - v.Position).Magnitude <= 4 then
 								break
 							end
 
 							if n ~= 1 then
+								--for better pathfinding feeling, skip the first iteration
 								local allow = 0
 
 								Hum:MoveTo(y.Position)
-
+								--jump if needed
 								if y.Action == Enum.PathWaypointAction.Jump then
 									Hum.Jump = true
 								end
-
+								--wait until reached waypoint or timeout
 								while (Torso.Position - y.Position).magnitude > 1.5 and allow < 25 do
 									allow = allow + 1
 									game:GetService("RunService").Heartbeat:wait()
 								end
-
+								--waypoint representation
 								local WaypointPart = Instance.new("Part")
 								WaypointPart.Anchored = true
 								WaypointPart.CanCollide = false
@@ -107,42 +120,45 @@ task.spawn(function()
 					end
 				end
 			else
+				--go to last position player was seen/go to player
 				if TargetCharacter or char:GetAttribute("AggroPlayer") == "LastPos" then
 					--go to last pos
 					path:ComputeAsync(Torso.Position, waypos)
 					local Waypoints = path:GetWaypoints()
-
+					--if the player is in freefall, skip pathfinding
 					if TargetCharacter and TargetCharacter.Humanoid:GetState() == Enum.HumanoidStateType.Freefall then
 						continue
 					end
-
+					--if path is successful, go through each waypoint towards target, confirm target is alive
 					if path.Status == Enum.PathStatus.Success and not (TargetCharacter and TargetCharacter.Humanoid.Health <= 0)  then
 						for i,v:PathWaypoint in Waypoints do
 							if not char:GetAttribute("AggroPlayer") then
+								--if aggro player attribute is cleared, stop pathfinding
 								print("give up gng :sob::finger_cross:")
 								Hum:MoveTo(Torso.Position)
 								break
 							end
 							if i ~= 1 then
-
+								--for better pathfinding feeling, skip the first iteration
 								local allow = 0
+								--go towards target, else go to last position known
 								if TargetCharacter then
 									Hum:MoveTo(v.Position, TargetCharacter.Torso)
 								else
 									Hum:MoveTo(LastPositionPlayerWasSeen)
 								end
-
-
+								--jump if needed
 								if v.Action == Enum.PathWaypointAction.Jump then
 									print("Jump")
 									Hum.Jump = true
 								end
 								--3.8
+								--wait until reached waypoint or timeout)
 								while (Torso.Position - v.Position).magnitude > 3.8 and allow <= 20 do
 									allow = allow + 1
 									game:GetService("RunService").Heartbeat:wait()
 								end
-
+								--waypoint representation
 								local WaypointPart = Instance.new("Part")
 								WaypointPart.Anchored = true
 								WaypointPart.CanCollide = false
@@ -155,6 +171,7 @@ task.spawn(function()
 							end
 						end
 					end
+					--if no path found, print and break
 				elseif path.Status == Enum.PathStatus.NoPath then
 					print("no path found")
 					break
@@ -166,28 +183,34 @@ task.spawn(function()
 	end
 end)
 
---raycast
+--raycasts
 
 while task.wait(0.05) do
+	--create raycasts
 	local RayCasts, VisualRayCasts, SuccessRays = NPC:CreateRayCasts(25) -- Raycast, VisualCast
-
+	--if tries exceed the maximun tries
 	if CatchPlayerTries >= MAX_TRIES then
-		--print("Go to breadcumbs (where player was last seen) and afterwards roam")
-		--create waypoints that'll go to breadcrumbs, and make the pathfind system flexible with attribute
-		--so it'll pathfind its way onto each waypoint of the breadcrumbs
-		--dont make it pathfind in ai script dumbass
+		--[[print("Go to breadcumbs (where player was last seen) and afterwards roam")
+		create waypoints that'll go to breadcrumbs, and make the pathfind system flexible with attribute
+		so it'll pathfind its way onto each waypoint of the breadcrumbs
+		dont make it pathfind in ai script dumbass]]
+		--^^^^^^^^^^ self comments
 
+		--if it has a last position seen
 		if NPC.LastPositionPlayerWasSeen then
+			--if visual last player seen debug option
 			if NPC.VisualLPS then
+				--less lag debug option, makes sure they don't clump together, alteast 0.5 studs away
 				if CheckForLastLPSPosition and NPC.LastLPSRegistered and (NPC.LastLPSRegistered - NPC.LastPositionPlayerWasSeen).Magnitude > 0.5 or CheckForLastLPSPosition and not NPC.LastLPSRegistered then
 					NPC:CreateVisualLPS()
 				elseif not CheckForLastLPSPosition then
 					NPC:CreateVisualLPS()
 				end
 			end
-
-			char:SetAttribute("AggroPlayer", "LastPos")		
+			--goes towards the last position
+			char:SetAttribute("AggroPlayer", "LastPos")
 		else
+			--if the npc doesn't have last position seen and aggro isn't roam, change it to roam 
 			if char:GetAttribute("AggroPlayer") ~= "Roam" then
 				print("roam through map")
 				char:SetAttribute("AggroPlayer", nil)
@@ -197,36 +220,42 @@ while task.wait(0.05) do
 			end
 		end
 
+		--reset tries
 		CatchPlayerTries = 0
 		print("set nil")
 	elseif OOP.IsPlayerInTable(SuccessRays) then
+		--if player found through raycasts, reset tries and aggro onto player
 		CatchPlayerTries = 0
 		char:SetAttribute("AggroPLayer", nil) --stop following whoever it was following
 	else
+		--if no player found nor max tries, increment tries and go to last position seen if it has plr
 		CatchPlayerTries+=1
 		if NPC.LastPositionPlayerWasSeen then
 			print("Player was last seen somewhere")
 			if NPC.VisualLPS then
+				--less lag debug option, makes sure they don't clump together, alteast 0.5 studs away
 				if CheckForLastLPSPosition and NPC.LastLPSRegistered and (NPC.LastLPSRegistered - NPC.LastPositionPlayerWasSeen).Magnitude > 0.5 or CheckForLastLPSPosition and not NPC.LastLPSRegistered then
 					NPC:CreateVisualLPS()
 				elseif not CheckForLastLPSPosition then
+					--debug option off
 					NPC:CreateVisualLPS()
 				end
 			end
-
+			--set aggro to last pos
 			char:SetAttribute("AggroPlayer", "LastPos")
 		end
+		--lil wait to prevent overload
 		game:GetService("RunService").Heartbeat:wait()
 	end
-
+	--turn each successfull ray red, successfull raycasts are the ones that hit any object
 	for i, Raycast: RaycastResult in SuccessRays do
 		if Raycast[1] then
 			if VisualRayCasts[i] then
 				VisualRayCasts[i].BrickColor = BrickColor.new("Really red")
 			end
-
+			--get character from raycast hit instance
 			local TargetCharacter = OOP.GetCharacter(Raycast[1].Instance)
-			
+			--if player was found and has a living humanoid, set aggro onto them and register last position seen
 			if TargetCharacter and TargetCharacter:FindFirstChild("Humanoid") and TargetCharacter.Humanoid.Health > 0 then
 				char:SetAttribute("AggroPlayer", TargetCharacter.Name)
 				NPC.LastPositionPlayerWasSeen = TargetCharacter.Torso.Position
@@ -235,26 +264,32 @@ while task.wait(0.05) do
 	end
 end
 
---module
+---------------------------------------------------------------------------------------------------------
+--OOP Module
 
 --!nocheck
 local TweenService = game:GetService("TweenService")
 
+--oop settings
 local module = {}
 module.__index = module
 
+--configurable variables
 module.NumberOfRays = 1
 module.FovDistance = 60
 module.Tilt = -1
 module.TOTAL_ANGLE = 90
 
+--roam waypoints
 module.RoamWayPoints = workspace.Waypoints
 
+--filters any glass and the NPC itself
 module.FilterTable = {script.Parent}
 local RayParams = RaycastParams.new()
 RayParams.FilterType = Enum.RaycastFilterType.Exclude
 RayParams.FilterDescendantsInstances = module.FilterTable
 
+--constructor
 function module.New(model: Model, VisualRayCast, VisualLPS)
 	local self = setmetatable({}, module)
 	self.Character = model
@@ -271,6 +306,7 @@ function module.New(model: Model, VisualRayCast, VisualLPS)
 	return self
 end
 
+--extra functions
 function module.OrderTable(Table)
 	local orderedtable = {}
 	for i,v in pairs(Table) do
@@ -309,6 +345,7 @@ function module.GetTableLength(Table)
 	return totalnumber
 end
 
+--creates a single raycast at a given degree
 function module:Create_Cast(deg)
 	local RayResult = workspace:Raycast(self.Head.Position, self.Head.CFrame:ToWorldSpace(CFrame.Angles(math.rad(module.Tilt), math.rad(deg), 0)).LookVector * module.FovDistance, RayParams)
 	local HitPosition = RayResult and RayResult.Position or self.Head.Position + self.Head.CFrame:ToWorldSpace(CFrame.Angles(math.rad(module.Tilt), math.rad(deg), 0)).LookVector * module.FovDistance
@@ -316,6 +353,7 @@ function module:Create_Cast(deg)
 	return RayResult, HitPosition
 end
 
+--turns the npc head 180 degrees and back (not currently used, planning on implementing it later)
 function module:TurnHead()
 	local Original_Orientation = self.Head.Orientation
 
@@ -368,6 +406,7 @@ function module:CreateRayCast(deg)
 	return {RayResult, HitPosition}
 end
 
+--creates direction vector, middle point and distance for visual raycast representation
 function module:CreateVectors(Hitpos)
 	local Directionvector = Hitpos - self.Head.Position
 	local MiddlePoint = self.Head.Position + Directionvector * 0.5
@@ -376,6 +415,7 @@ function module:CreateVectors(Hitpos)
 	return Directionvector, MiddlePoint, Distance
 end
 
+--creates the visual raycast using the vectors, the id being the index of the raycast
 function module:CreateVisualRayCast(HitPosition, MiddlePoint, Distance, ID)
 	local VisualRay = Instance.new("Part")
 	table.insert(module.FilterTable, VisualRay)
@@ -392,41 +432,50 @@ function module:CreateVisualRayCast(HitPosition, MiddlePoint, Distance, ID)
 	return VisualRay
 end
 
+--better function for creating multiple raycasts, automatically divides the total angle by the amount of raycasts to make it easier
 function module:CreateRayCasts(amount)
+	--clear all previous visual rays
 	for i,v in pairs(workspace:GetDescendants()) do
 		if v.Name:match("HeadRayCast") then
 			v:Destroy()
 		end
 	end
-	
+	--angle per each cast
 	local AnglePerEachCast = module.TOTAL_ANGLE / amount
-	
+
+	--variables
 	local RayTable = {}
 	local VisualTable = {}
 	local SuccessTable = {}
 	local Directionvector, MiddlePoint, Distance, VisualCast
 	
-	local CurrentAngle = 45
+	--stars from 45 degrees to -45 degrees
+	local CurrentAngle = modul.TOTAL_ANGLE / 2
 	
+	--creates each raycast with designated angle
 	for i = 1, amount, 1 do
 		local RayResult, HitPosition
 		if i == 1 then
+			--45 degrees first cast
 			RayResult, HitPosition = self:Create_Cast(CurrentAngle)
 		else
+			--subsequent casts
 			RayResult, HitPosition = self:Create_Cast(CurrentAngle - AnglePerEachCast)
 		end
-		
+		--creates vectors for each cast
 		Directionvector, MiddlePoint, Distance = self:CreateVectors(HitPosition)
-		
+
+		--creates visual raycast if enabled
 		if self.VisualRayCast then
 			VisualCast = self:CreateVisualRayCast(HitPosition, MiddlePoint, Distance, i)
 			VisualTable[i] = VisualCast
 		end
-		
+
+		--if raycast hit something, add it to success table
 		if RayResult then
 			SuccessTable[i] = {RayResult}
 		end
-		
+		--sends the raycast index with its result and hit position
 		RayTable[i] = {RayResult, HitPosition}
 		CurrentAngle -= AnglePerEachCast
 	end
@@ -434,6 +483,7 @@ function module:CreateRayCasts(amount)
 	return RayTable, VisualTable, SuccessTable
 end
 
+--creates visual last position seen
 function module:CreateVisualLPS()
 	local LastPartSeen = Instance.new("Part")
 	table.insert(module.FilterTable, LastPartSeen)
